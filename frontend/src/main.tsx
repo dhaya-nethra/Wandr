71292d44ca5a585843919f5c98b4f7f9c94c5ddd
Alamelu <alamelu448@gmail.com>
2026-05-14 16:08:26 +0530

    final changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,10 @@
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.href) {
+    history.replaceState(null, "", redirect);
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
